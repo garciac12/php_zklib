1da18bea98ee9cad388bbd253948a78e071f8ff9
Carlos A. Garcia <garciac12@gmail.com>
2017-01-17 15:37:43 -0300

    creacion de clases

---
+++ src/ZK.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: cgarcia
+ * Date: 17/01/17
+ * Time: 15:28
+ */
+class ZK
+{
+    private $url;
+    private $port;
+    private $password;
+
+    public function __construct($url, $port, $password)
+    {
+
+    }
+}
